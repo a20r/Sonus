56318b2610006a742c61903f670a7f5febe55f83
pruthvikar reddy <pruthvikar@gmail.com>
2014-03-15 18:37:44 +0000

    fb login

--- static/js/fb.js
+++ static/js/fb.js
@@ -16,7 +16,8 @@
             // The response object is returned with a status field that lets the app know the current
             // login status of the person. In this case, we're handling the situation where they 
             // have logged in to the app.
-            testAPI();
+             console.log('authorized');
+            changeName();
           } else if (response.status === 'not_authorized') {
             // In this case, the person is logged into Facebook, but not into the app, so we call
             // FB.login() to prompt them to do so. 
@@ -25,14 +26,16 @@
             // (1) JavaScript created popup windows are blocked by most browsers unless they 
             // result from direct interaction from people using the app (such as a mouse click)
             // (2) it is a bad experience to be continually prompted to login upon page load.
-            FB.login();
+             console.log('not authorized');
+
           } else {
             // In this case, the person is not logged into Facebook, so we call the login() 
             // function to prompt them to do so. Note that at this stage there is no indication
             // of whether they are logged into the app. If they aren't then they'll see the Login
             // dialog right after they log in to Facebook. 
             // The same caveats as above apply to the FB.login() call here.
-            FB.login();
+             console.log('not logged in');
+
           }
         });
         };
@@ -48,9 +51,18 @@
 
         // Here we run a very simple test of the Graph API after login is successful. 
         // This testAPI() function is only called in those cases. 
-        function testAPI() {
+        function changeName() {
           console.log('Welcome!  Fetching your information.... ');
           FB.api('/me', function(response) {
             console.log('Good to see you, ' + response.name + '.');
+            $('#FBNAME').text(' Welcome, '+response.name);
+            
           });
+          
+          $('.fa-facebook-square').click(function() {
+              FB.login();
+          });
+          
+          
+          
         }
